d3c8e4e9266d4f2cea28249f1d2a2d8f7dc69fca
vivekd31 <drakshapallyvivek31@gmail.com>
2022-01-26 14:47:09 -0600

    Initial

---
+++ 01mapper.py
@@ -0,0 +1,15 @@
+# Vivek Drakshapally
+# Practice Mapper
+
+f = open("purchases.txt","r")  # open file, read-only
+o = open("out01.txt", "w") # open file, write
+for line in f:  
+    rowList = line.strip().split("    ") 
+    print (rowList )
+    print (len(rowList ))
+    if len(rowList) == 6:
+        date, time, location, dept, amount, payType = rowList  #assign names
+        print ("{0}\t{1}".format(location, amount))
+        o.write("{0}\t{1}\n".format(location, amount))
+f.close()
+o.close()
